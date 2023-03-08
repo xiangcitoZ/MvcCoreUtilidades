ab736b5e2ae15a6301d8818eb9608590fa987e0e
xiangcitoZ <pzxiang1@gmail.com>
2023-03-08 13:12:29 +0100

    08/03

--- MvcCoreUtilidades/Controllers/CifradosController.cs
+++ MvcCoreUtilidades/Controllers/CifradosController.cs
@@ -1,12 +1,76 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreUtilidades.Helpers;
 
 namespace MvcCoreUtilidades.Controllers
 {
     public class CifradosController : Controller
-    {
-        public IActionResult Index()
+{   
+    public IActionResult CifradoEficiente()
         {
             return View();
         }
+
+    [HttpPost]
+    public IActionResult CifradoEficiente
+            (string contenido, string resultado, string accion)
+        {
+            if(accion.ToLower() == "cifrar")
+            {
+                //LO QUE HAREMOS SERA GUARDAR EL SALT GENERADO
+                string response =
+                    HelperCryptography.EncriptarContenido(contenido, false);
+                ViewData["TEXTOCIFRADO"] = response;
+
+            }else if (accion.ToLower() == "comparar")
+            {
+                string response =
+                    HelperCryptography.EncriptarContenido(contenido, true);
+                if(response != resultado)
+                {
+                    ViewData["MENSAJE"] = "<h1 style ='color:red'>No son iguales </h1> ";
+
+                }
+                else
+                {
+                    ViewData["MENSAJE"] = "<h1 style ='color:blue'>Son iguales </h1> ";
+                }
+            }
+
+
+            return View();
+        }
+    
+
+    public IActionResult CifradoBasico()
+    {
+        return View();
     }
+ 
+    [HttpPost]
+    public IActionResult CifradoBasico
+        (string contenido, string resultado, string accion)
+    {
+        //CIFRAMOS EL CONTENIDO CON EL HELPER
+        string response =
+            HelperCryptography.EncriptarTextoBasico(contenido);
+        if (accion.ToLower() == "cifrar")
+        {
+            ViewData["TEXTOCIFRADO"] = response;
+        }else if (accion.ToLower() == "comparar")
+        {
+            //COMPARAMOS EL CONTENIDO CIFRADO CON EL RESULTADO
+            //DE LA CAJA
+            if (response != resultado)
+            {
+                ViewData["MENSAJE"] = "Los valores no coinciden";
+            }
+            else
+            {
+                ViewData["MENSAJE"] = "Contenidos iguales!!!";
+            }
+        }
+        return View();
+    }
+}
+
 }
--- MvcCoreUtilidades/Helpers/HelperCryptography.cs
+++ MvcCoreUtilidades/Helpers/HelperCryptography.cs
@@ -4,23 +4,80 @@ namespace MvcCoreUtilidades.Helpers
 {
     public class HelperCryptography
     {
+        public static string Salt { get; set; }
+        public static string GenerateSalt()
+        {
+            Random random = new Random();
+            string salt = "";
+            for(int i = 1; i<= 50; i++)
+            {
+                int aleat = random.Next(0, 255);
+                char letra = Convert.ToChar(aleat);
+                salt += letra;
+            }
+            return salt;
+
+        }
+
+        public static string EncriptarContenido
+            (string contenido, bool comparar)
+        {
+            if(comparar == false)
+            {
+                //GENERAMOS NUESTRO SALT
+                Salt = GenerateSalt();
+            }
+            //EL SALT LO INCLUIREMOS DONDE DESEEMOS
+            //INCLUIMOS EL SALT AL FINAL DEL PASSWORD A CIFRAR
+            string contenidosalt = contenido + Salt;
+            SHA256Managed sHA256 = new SHA256Managed();
+
+            byte[] salida;
+            UnicodeEncoding encoding = new UnicodeEncoding();
+            //CONVERTIMOS A BYTES EL CONTENIDO DEL SALT
+            salida = encoding.GetBytes(contenidosalt);  
+            //DEBEMOS REALIZAR EL CIFRADO SOBRE CIFRADO N VECES
+            for(int i = 1; i<= 55; i++) 
+            {
+                //REALIZAMOS EL CIFRADO
+                salida = sHA256.ComputeHash(salida);
+                
+            }
+            //LIMPIAMOS EL SHA256
+            sHA256.Clear();
+            string resultado = encoding.GetString(salida);
+            return resultado;
+        }
+
+
+
+
+        //CREAMOS UN METODO STATIC PARA CIFRAR UN STRING
+        //DE FORMA MUY BASICA.  DEVOLVEMOS EL STRING GENERADO
         public static string EncriptarTextoBasico(string contenido)
         {
+            //NECESITAMOS TRABAJAR A NIVEL DE BYTE[]
+            //DEBEMOS CONVERTIR EL TEXTO RECIBIDO A BYTE[]
             byte[] entrada;
+            //UNA VEZ CONVERTIDO EL CIFRADO, NOS DEVUELVE 
+            //UN BYTE[] DE SALIDA
             byte[] salida;
-
+            //NECESITAMOS UN CONVERSOR PARA CONVERTIR STRING A BYTE[]
+            //Y VICEVERSA
             UnicodeEncoding encoding = new UnicodeEncoding();
+            //NECESITAMOS UN OBJETO PARA EL CIFRADO
             SHA1Managed sHA1 = new SHA1Managed();
-
+            //CONVERTIMOS EL OBJETO STRING A BYTE[]
             entrada = encoding.GetBytes(contenido);
+            //EL OBJETO SHA1 RECIBE UN ARRAY DE BYTES E
+            //INTERNAMENTE HACE "ALGO" PARA DEVOLVER OTRO 
+            //ARRAY DE BYTES
             salida = sHA1.ComputeHash(entrada);
-
+            //CONVERTIMOS BYTES[] DE SALIDA A STRING
             string resultado = encoding.GetString(salida);
             return resultado;
 
 
         }
-
-
     }
 }
